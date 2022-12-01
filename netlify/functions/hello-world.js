5def7dd0686f76c073c62ae7e4cea71c23058b7e
RadicaIEdward <edward.o.galindez@gmail.com>
2022-12-01 18:20:24 -0500

    Install Chalk and Luxon, update hello-world

--- netlify/functions/hello-world.js
+++ netlify/functions/hello-world.js
@@ -1,4 +1,9 @@
+const chalk = require('chalk');
+const { DateTime } = require('luxon');
+
 exports.handler = async function () {
+  const date = DateTime.now();
+  console.log(chalk.cyanBright(`Hello Professor Teeters! This a timestamp: ${date} `));
   return {
     statusCode: 200,
     body: JSON.stringify({
